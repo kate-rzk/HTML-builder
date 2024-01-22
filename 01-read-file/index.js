4a0dbb6a6edabb28fa98e00923dde70d208f7f82
kate-rzk <razkovaekaterina@gmail.com>
2024-01-22 14:45:55 +0300

    feat: add solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath);
+
+readStream.on('data', (chunk) => {
+  console.log(chunk.toString());
+});
+
+readStream.on('end', () => {
+  console.log('File reading is complete');
+});
+
+readStream.on('error', (err) => {
+  console.error(`An error occurred: ${err.message}`);
+});
